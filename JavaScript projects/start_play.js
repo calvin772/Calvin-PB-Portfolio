5da30d31632e24dd02dee0049dd9dbd6db93da9d
calvin772 <bor21003@byui.edu>
2026-04-20 18:20:08 -0600

    started start_play.js file

--- JavaScript projects/start_play.js
+++ JavaScript projects/start_play.js
@@ -1,1 +1,23 @@
-print("hello world")
+
+// Variables: 
+const username = "Calvin"; //pernament variable
+let age = 23; //can change later
+
+console.log(age)
+console.log(username);
+
+// Asynchronous Behavior & Timing Responses:
+console.log("start");
+
+setTimeout(() =>{
+    console.log("waiting 2 seconds");
+}, 2000); //1 second = 1000(ie: 2000 for 2 seconds)
+
+console.log("end");
+
+// Arrow Functions:
+const add = (a,b) => {
+    return a + b;
+};
+
+console.log(add(5,22))
